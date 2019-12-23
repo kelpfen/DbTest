ff42da6c83995fe63f2c2636b6b04d9d946ee971
kelpfen <lwkelpfen@gmail.com>
2019-12-23 14:46:40 +0800

    Initial commit

---
+++ app/src/main/java/com/example/dbtest/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.dbtest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.FragmentTransaction
+import com.example.dbtest.Database.AppDB
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    var mgr  = supportFragmentManager
+
+    //todo:: declare AppDB reference
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //todo:: create AppDB instance
+
+        btnAdd.setOnClickListener() {
+            mgr.beginTransaction().replace(R.id.mainFrame, AddRecordFragment()).commit()
+        }
+
+        btnView.setOnClickListener() {
+            mgr.beginTransaction().replace(R.id.mainFrame, ViewRecordFragment()).commit()
+        }
+    }
+
+}
